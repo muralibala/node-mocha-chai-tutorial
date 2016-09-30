8bcd09dddbb215b1876173deb09328c26a12a437
Murali Bala <muralibala@westat.com>
2016-09-30 13:47:44 -0400

    updated the config for mongo to use 127.0.0.1

--- server/_config.js
+++ server/_config.js
@@ -3,7 +3,7 @@ var config = {};
 config.mongoURI = {
   development: 'mongodb://localhost/node-testing',
   test: 'mongodb://localhost/node-test',
-  prod: 'mongodb://localhost:27017/node-prod'
+  prod: 'mongodb://127.0.0.1:27017/node-prod'
 };
 
 module.exports = config;
